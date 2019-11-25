2914553afaa901babf9b245347808341226a7edc
samtheBrave <siaboonleong@hotmail.com>
2019-11-25 22:37:24 +0800

    Update QuestionView.js

--- src/components/QuestionView.js
+++ src/components/QuestionView.js
@@ -30,7 +30,7 @@ class QuestionView extends Component {
   getQuestions = () => {
     $.ajax({
      
-      url: `/questions?page_num=${this.state.page}`, //TODO: update request URL
+      url: `https://triviasiabl.herokuapp.com/questions?page_num=1/questions?page_num=${this.state.page}`, //TODO: update request URL
       type: "GET",
       headers: {
         "accept": "application/json",
