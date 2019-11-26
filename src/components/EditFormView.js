cd2aa54232287f3274d526ac50b30fe9ba03e31a
samtheBrave <siaboonleong@hotmail.com>
2019-11-26 15:14:45 +0800

    t

--- src/components/EditFormView.js
+++ src/components/EditFormView.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import $ from 'jquery';
 import '../stylesheets/EditFormView.css';
 import Select  from 'react-dropdown-select'
-const backend = 'https://triviasiabl.herokuapp.com/';
+const backend = 'https://cors-anywhere.herokuapp.com/https://triviasiabl.herokuapp.com';
 
 class EditFormView extends Component {
   constructor(props){
--- src/components/FormView.js
+++ src/components/FormView.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import $ from 'jquery';
 import '../stylesheets/FormView.css';
 import Select  from 'react-dropdown-select'
-const backend = 'https://triviasiabl.herokuapp.com/';
+const backend = 'https://cors-anywhere.herokuapp.com/https://triviasiabl.herokuapp.com';
 
 class FormView extends Component {
   constructor(props){
